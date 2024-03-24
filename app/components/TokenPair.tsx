a8e7a1904b15c1a9c5f8dd819d5309b27a02dad6
vividwood <101632255+vividwood@users.noreply.github.com>
2024-03-24 19:04:30 +0100

    experient: no tw

--- app/components/TokenPair.tsx
+++ app/components/TokenPair.tsx
@@ -9,30 +9,80 @@ export interface TokenProps {
 
 export function TokenCard({ token }: { token: TokenProps }) {
   // console.log(token)
-  return <div tw="flex flex-col border-2 flex-1 items-center space-x-4 rtl:space-x-reverse">
-    <div tw="flex text-4xl">
-      <p tw="truncate font-medium text-gray-900 dark:text-white truncate">{token.name}</p>
-      <p tw="truncate ml-2 text-gray-500 dark:text-gray-400">{token.symbol}</p>
+  return <div style={{"display":"flex","marginLeft":"1rem","flexDirection":"column","flex":"1 1 0%","alignItems":"center","borderWidth":"2px"}}>
+    <div style={{"display":"flex","fontSize":"2.25rem","lineHeight":"2.5rem"}}>
+      <p style={{"fontWeight":500,"color":"#111827","overflow":"hidden","textOverflow":"ellipsis","whiteSpace":"nowrap"}}>{token.name}</p>
+      <p style={{"marginLeft":"0.5rem","color":"#6B7280","overflow":"hidden","textOverflow":"ellipsis","whiteSpace":"nowrap"}}>{token.symbol}</p>
     </div>
-    <div tw="flex">
-      <img tw="h-60 w-60 rounded-full" src={token.logo?.small || `${process.env.NEXT_PUBLIC_DOMAIN}/empty.png`} />
+    <div style={{"display":"flex"}}>
+      <img style={{"borderRadius":"50%", width: "15rem", height: "15rem"}}  src={token.logo?.small || `${process.env.NEXT_PUBLIC_DOMAIN}/empty.png`} />
     </div>
   </div>
 }
 
 export function TokenPair({ tokenList }: { tokenList: TokenProps[] }) {
   const [token0, token1] = tokenList;
-  return <div tw="flex  w-full h-full flex-row items-center justify-between p-24">
-    <div tw="flex flex-1 max-w m-8 rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
-      <div tw="flex w-full flex-1 p-8">
-        <ul tw="flex flex-row flex-1  divide-y divide-gray-200 dark:divide-gray-700">
+  return <div style={{
+    "display": "flex",
+    "padding": "6rem",
+    "flexDirection": "row",
+    "justifyContent": "space-between",
+    "alignItems": "center",
+    "width": "100%",
+    "height": "100%",
+  }}>
+    <div style={{
+      "display": "flex",
+      "padding": "1.5rem",
+      "margin": "2rem",
+      "backgroundColor": "#ffffff",
+      "flex": "1 1 0%",
+      "borderRadius": "0.5rem",
+      "borderWidth": "1px",
+      "borderColor": "#E5E7EB",
+      "boxShadow": "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+    }}>
+      <div style={{
+        display: "flex",
+        padding: "2rem",
+        flex: "1 1 0%",
+        width: "100%",
+      }}>
+        <ul style={{
+          display: "flex", 
+          flexDirection: "row", 
+          flex: "1 1 0%", 
+          borderTopWidth: "1px", 
+          borderColor: "#E5E7EB", 
+        }}>
           <TokenCard token={token0}/>
         </ul>
       </div>
     </div>
-    <div tw="flex flex-1 max-w m-8 rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
-      <div tw="flex w-full flex-1 p-8">
-        <ul tw="flex flex-row flex-1  divide-y divide-gray-200 dark:divide-gray-700">
+    <div style={{
+      "display": "flex",
+      "padding": "1.5rem",
+      "margin": "2rem",
+      "backgroundColor": "#ffffff",
+      "flex": "1 1 0%",
+      "borderRadius": "0.5rem",
+      "borderWidth": "1px",
+      "borderColor": "#E5E7EB",
+      "boxShadow": "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+    }}>
+      <div  style={{
+        display: "flex",
+        padding: "2rem",
+        flex: "1 1 0%",
+        width: "100%",
+      }}>
+        <ul style={{
+          display: "flex", 
+          flexDirection: "row", 
+          flex: "1 1 0%", 
+          borderTopWidth: "1px", 
+          borderColor: "#E5E7EB", 
+        }}>
           <TokenCard token={token1}/>
         </ul>
       </div>
