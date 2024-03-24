b1a6e5279a83222f6d002328c6e9209c7248e882
vividwood <101632255+vividwood@users.noreply.github.com>
2024-03-24 19:25:53 +0100

    optimize styles

--- app/components/TokenPair.tsx
+++ app/components/TokenPair.tsx
@@ -9,14 +9,10 @@ export interface TokenProps {
 
 export function TokenCard({ token }: { token: TokenProps }) {
   // console.log(token)
-  return <div style={{"display":"flex","marginLeft":"1rem","flexDirection":"column","flex":"1 1 0%","alignItems":"center","borderWidth":"2px"}}>
-    <div style={{"display":"flex","fontSize":"2.25rem","lineHeight":"2.5rem"}}>
-      <p style={{"fontWeight":500,"color":"#111827","overflow":"hidden","textOverflow":"ellipsis","whiteSpace":"nowrap"}}>{token.name}</p>
-      <p style={{"marginLeft":"0.5rem","color":"#6B7280","overflow":"hidden","textOverflow":"ellipsis","whiteSpace":"nowrap"}}>{token.symbol}</p>
-    </div>
-    <div style={{"display":"flex"}}>
-      <img style={{"borderRadius":"50%", width: "15rem", height: "15rem"}}  src={token.logo?.small || `${process.env.NEXT_PUBLIC_DOMAIN}/empty.png`} />
-    </div>
+  return <div style={{ "display": "flex", "flexGrow": "1", "marginLeft": "1rem", "flexDirection": "column", "alignItems": "center" }}>
+    <p style={{ "color": "#111827" }}>{token.name}</p>
+    <p style={{ "marginLeft": "0.5rem", "color": "#6B7280" }}>{token.symbol}</p>
+    <img style={{ "borderRadius": "50%", width: "15rem", height: "15rem" }} src={token.logo?.small || `${process.env.NEXT_PUBLIC_DOMAIN}/empty.png`} />
   </div>
 }
 
@@ -42,22 +38,7 @@ export function TokenPair({ tokenList }: { tokenList: TokenProps[] }) {
       "borderColor": "#E5E7EB",
       "boxShadow": "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
     }}>
-      <div style={{
-        display: "flex",
-        padding: "2rem",
-        flex: "1 1 0%",
-        width: "100%",
-      }}>
-        <ul style={{
-          display: "flex", 
-          flexDirection: "row", 
-          flex: "1 1 0%", 
-          borderTopWidth: "1px", 
-          borderColor: "#E5E7EB", 
-        }}>
-          <TokenCard token={token0}/>
-        </ul>
-      </div>
+      <TokenCard token={token0} />
     </div>
     <div style={{
       "display": "flex",
@@ -70,22 +51,7 @@ export function TokenPair({ tokenList }: { tokenList: TokenProps[] }) {
       "borderColor": "#E5E7EB",
       "boxShadow": "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
     }}>
-      <div  style={{
-        display: "flex",
-        padding: "2rem",
-        flex: "1 1 0%",
-        width: "100%",
-      }}>
-        <ul style={{
-          display: "flex", 
-          flexDirection: "row", 
-          flex: "1 1 0%", 
-          borderTopWidth: "1px", 
-          borderColor: "#E5E7EB", 
-        }}>
-          <TokenCard token={token1}/>
-        </ul>
-      </div>
+      <TokenCard token={token1} />
     </div>
 
   </div>
